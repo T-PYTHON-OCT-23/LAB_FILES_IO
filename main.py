ca09683feeeedcb97c8b877a8c46904e264f323d
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-23 12:12:38 +0300

    fix bugs &&& easy

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 while True:    
-    ans=input("do you want to add a new To-Do item y/n or exit")
+    ans=input("do you want to add a new To-Do item y/n or exit ")
     if ans.lower()=="exit":
         break
     if ans.lower()=="n" or ans.lower()=="no":
-        ans=input("Do you want to list your items?")
+        ans=input("Do you want to list your items? ")
         if ans.lower()=="n" or ans.lower()=="no":
             break
         elif ans.lower()=="y" or ans.lower()=="yes":
             with open("to_do.txt","r",encoding="UTF-8")as f:
-                 print(f.readline())
+                for i in f.readlines():
+                    print(i)
     elif ans.lower()=="y" or ans.lower()=="yes":
-        ans=input("type in To-Do item")
-        with open("to_do.txt","wt+",encoding="UTF-8") as f: 
-             f.write(ans)
+        ans=input("type in To-Do item ")
+        with open("to_do.txt","a",encoding="UTF-8") as f:
+            f.write(ans + "\n") # add a new line after the new item
