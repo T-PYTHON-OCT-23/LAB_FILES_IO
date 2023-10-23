8746c0c8960b8c8b215bde2e74f72ed13e56937d
Anoud <Anoudi90@icloud.com>
2023-10-23 13:14:59 +0300

    edit

--- LAB_FILES_IO.py
+++ LAB_FILES_IO.py
@@ -4,18 +4,19 @@
 
 
 file = open("to-do.txt")
-answer = input ("\n do you want to add a new To-Do item? \n  answer by \"y\" for yes and \"n\" for no ")
+answer = ""
 while answer != "exit":
     answer = input ("\n do you want to add a new To-Do item? \n  answer by \"y\" for yes and \"n\" for no ")
     print(answer)
     if answer == "y":
         file = open("to-do.txt" , "a" , encoding="utf-8")
-        new_item = file.write(input("type in your new To-Do item : " + "\n" )) #in new line
+        new_item = file.write(input("type in your new To-Do item : "  ) + "\n" ) #in new line
         file.close()
     elif answer == "n":
         answer=(input("do you want to list your To-Do items ? \n answer \"y\" for yes and \"n\" for no."))
         print(answer)
         if answer == "y": #readlines()
+            open("to-do.txt" , "r" , encoding="utf-8")
             file.seek(0)
             list_lines = file.readlines()
             print(list_lines)
