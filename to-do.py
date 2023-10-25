ff83cf5c60685e7b15e96881c061ecf02426c686
lama1-abdullah <148319427+lama1-abdullah@users.noreply.github.com>
2023-10-25 23:11:35 +0300

    Update to-do.py
    done

--- to-do.py
+++ to-do.py
@@ -1,30 +1,22 @@
-while True: 
- 
-    response =input("do you want to add a new To-Do item? \n answer by \"y\" for yes  and \"n\" for no: " )
- 
-    if response == "exit":
-      break
-    elif response == "y" :
-  
-       new_item = input("Type in your new To-Do item: ")
-       with open("to-do.txt", "a", encoding="utf-8") as file:
-            file.write(new_item + '\n')
+while True:
+    response = input('Do you want to add a new To-Do item? Answer by "y" for yes and "n" for no. "exit" to exit: ')
+
+    if response == "y":
+        new_item = input("Type in your new To-Do item: ")
+        with open("to-do.txt", "a", encoding="utf-8") as file:
+            file.write(new_item + "\n")
+
     elif response == "n":
-     
-     list_response =input("do you want to list your To-Do items ?  answer by \"y\" for yes and \"n\" for no: " )
-    if list_response== "y" :
-       with open("to-do.txt", "a", encoding="utf-8")as file:
-          list_line_items= file.readline()
-          if list_line_items:
-           print("Your TO-DO List: ")  
-           for i, item in enumerate(list_line_items, 1):
-                        print(f"{i}. {item.strip()}")
-         # else:
-               # print("Your To-Do list is empty.")
+        list_response = input('Do you want to list your To-Do items? Answer "y" for yes and "n" for no: ')
+
+        if list_response == "y":
+            with open("to-do.txt", "r", encoding="utf-8") as file:
+                list_line_items = file.read()
+                print(list_line_items)
+
+    elif response == "exit":
+        print("\nThank you for using the To-Do program. Come back again soon.")
+   
     else:
-        print("Invalid input. Please enter 'y', 'n', or 'exit'.")
-    file.seek(0)     
-    file.close()
-
-print("\n Thank you for using the To-Do program. Come back again soon.")
-
+        print("Unrecognized input. Please enter 'y' to add, 'n' to list, or 'exit' to exit.")
+        break
